ef012f15b48b952bd9d9b04f6a432691f84e58a4
Asha Sanford <sanford.asha97@yahoo.com>
2021-07-14 18:51:58 -0400

    added resolvers to schema

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,22 @@
+const { User, Book } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async(parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({})
+                .select('-__v -password')
+                .populate(`books`)
+
+                return userData;
+
+            }
+
+            throw new AuthenticationError('Not logged in');
+
+        },
+    },
+}
+
