a846b4e55baaea7db967036ad78b4b4667dcc4cd
Asha Sanford <sanford.asha97@yahoo.com>
2021-07-14 20:15:29 -0400

    updated resolver in schema. created typeDefs in schema.

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,78 +1,84 @@
+
 const { User, Book } = require('../models');
-const { AuthenticationError } = require('apollo-server-express');
 const { signToken } = require('../utils/auth');
+const { AuthenticationError } = require('apollo-server-express');
 
 const resolvers = {
+
     Query: {
 
-        me: async(parent, args, context) => {
+        me: async (parent, args, context) => {
 
-            if (context.user) {
+            if(context.user) {
                 const userData = await User.findOne({})
                 .select('-__v -password')
-                .populate(`books`)
-
+                .populate('books')
+            
                 return userData;
-
             }
 
-            throw new AuthenticationError('Not logged in');
+            throw new AuthenticationError('Not logged in')
+
         },
+
     },
 
     Mutation: {
-        
+
         addUser: async (parent, args) => {
             const user = await User.create(args);
             const token = signToken(user);
-
-            return { token, user };
+          
+            return {token, user};
         },
 
-        login: aysnc (parent, { email, password }) => {
-            const user = await User.findOne({ email }); 
-            
-            if (!user) {
-                throw new AuthenticationError('Login incorrect');
+        login: async (parent, {email, password}) => {
+            const user = await User.findOne({email});
+
+            if(!user) {
+                throw new AuthenticationError('Login credentials incorrect!');
             }
 
             const correctPass = await user.isCorrectPassword(password);
 
             if(!correctPass) {
-                throw new AuthenticationError('Login incorrect');
+                throw new AuthenticationError('Login credentials incorrect!');
             }
 
             const token = signToken(user);
-            return { token, user };
+            return {token, user};
+    
         },
 
         saveBook: async (parent, args, context) => {
             if (context.user) {
-                const updatedUser = await User.findByIdAndUpdate(
-                    { _id: context.user._id },
-                    { $addToSet: { savedBooks: args.input } },
-                    { new: true }
-                );
-
-                return updatedUser;
-
+          
+             const updatedUser =  await User.findByIdAndUpdate(
+                { _id: context.user._id },
+                { $addToSet: { savedBooks: args.input } },
+                { new: true }
+              );
+          
+            return updatedUser;
             }
-
-            throw new AuthenticationError('You must be logged in!');
+          
+            throw new AuthenticationError('Please login first!');
         },
 
-        removeBook: aysnc (parent, args, context ) => {
-            if (context.user) {
-                const updatedUser = await User.findOneAndUpdate(
-                    { _id: context.user._id },
-                    { $pull: { savedBooks: { bookId: args.bookId } } },
-                    { new: true }
-                );
 
-                return updatedUser;
+
+        removeBook: async (parent, args, context) => {
+            if(context.user) {
+            const updatedUser = await User.findOneAndUpdate(
+                { _id: context.user._id },
+                { $pull: { savedBooks: { bookId: args.bookId } } },
+                { new: true }
+            );
+
+            return updatedUser;
             }
 
-            throw new AuthenticationError('You must be logged in!');
+            throw new AuthenticationError('Please login first!');
         }
     }
 };
